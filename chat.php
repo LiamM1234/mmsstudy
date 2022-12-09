82c1de1efebf2405fccf733726c775a448b46659
Liam <100875517+LiamM1234@users.noreply.github.com>
2022-12-09 20:27:47 +0000

    altered page titles

--- chat.php
+++ chat.php
@@ -40,7 +40,7 @@ function loginForm(){
   <meta name="viewport" content="width=device-width initial-scale=1">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>Chat</title>
+    <title>Study+ | Chat</title>
     <link href="chat.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png">
--- dictionary/index.php
+++ dictionary/index.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">  
-    <title>Study+ Dictionary</title>
+    <title>Study+ | Dictionary</title>
     <link rel="stylesheet" href="dictionary/dictionary.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
