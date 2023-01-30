cfcf8b961814d9dc33c9f133c791c344f335f4d3
Liam <100875517+LiamM1234@users.noreply.github.com>
2023-01-30 21:04:24 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mmsstudy
 📖 Study for your upcoming assessments
 💬 Chat with other users
-📘 Use a bulit-in dictionary
+📘 Use a built-in dictionary
 📑 Make a planner to stay organized
--- quiz/script.js
+++ quiz/script.js
@@ -126,16 +126,16 @@ function showResult(){
     quiz_box.classList.remove("activeQuiz");
     result_box.classList.add("activeResult");
     const scoreText = result_box.querySelector(".score_text");
-    if (userScore > 3){
-        let scoreTag = '<span>and congrats! , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+    if (userScore > 7){
+        let scoreTag = '<span>Congrats! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else if(userScore > 1){
-        let scoreTag = '<span>and nice , You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>and nice! You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else{
-        let scoreTag = '<span>and sorry , You got only <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>and sorry. You only got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
 }
